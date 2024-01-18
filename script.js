3e361d054f2136da714c63b02f1038724e000af2
ByronOgada <daone89008@gmail.com>
2024-01-18 07:53:18 -0800

    update0.3

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
- const aboutTitle = document.getElementById("title");
+//aboutByronO java
+    //variables
+const aboutTitle = document.getElementById("title");
+let count = 0;
+    //functions
+function showAdjectives (id){
+    document.getElementsById("id")="The Coolest, Smartes, and Funiest";
+}
+function changeImage(){
+    count=count+1;
+    switch (count % 3){
+        case 0:
+            document.getElementById("meImage").src="me1.png";
+            break;
+        case 1:
+            document.getElementById("meImage").src="me2.png";
+            break;
+        case 2:
+            document.getElementsById("meImage").src="me3.png";
+            break;
+    }
+    print(count);
+}
